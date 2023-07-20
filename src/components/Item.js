66b6f1bf80b8785b465da5c9e6357900310d947a
Sam .M <you@example.com>
2023-07-20 13:48:45 +0300

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [itemState, setItemState] = useState({
+    [name]: { isAdded: "", addedMessage: "Add to cart" },
+  });
+
+  function inCartClicked(e) {
+    const clickedName = e.target.getAttribute("datacount");
+    if (itemState[clickedName].isAdded === "") {
+      setItemState((prevState) => ({
+        ...prevState,
+        [clickedName]: {
+          ...prevState[clickedName],
+          isAdded: "in-cart",
+          addedMessage: "Remove From Cart",
+        },
+      }));
+    } else {
+      setItemState((prevState) => ({
+        ...prevState,
+        [clickedName]: {
+          ...prevState[clickedName],
+          isAdded: "",
+          addedMessage: "Add to cart",
+        },
+      }));
+    }
+  }
   return (
-    <li className="">
+    <li className={itemState[name].isAdded}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={inCartClicked} datacount={name}>
+        {itemState[name].addedMessage}
+      </button>
     </li>
   );
 }
